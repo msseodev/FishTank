46b3bc9e617c2ff123077bbbe10a9ef9b9cc672a
MS Seo <msseo>
2022-05-08 22:57:03 +0900

    [Arduino] Add json library.

---
+++ ClionFishTank/FishPacket.h
@@ -0,0 +1,13 @@
+//
+// Created by user on 2022-05-08.
+//
+
+#ifndef CLIONFISHTANK_FISHTANK_H
+#define CLIONFISHTANK_FISHTANK_H
+
+struct FishPacket {
+    int opCode;
+    double data;
+};
+
+#endif //CLIONFISHTANK_FISHTANK_H
