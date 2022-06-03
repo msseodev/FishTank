29cd1c5a3d59d4100d6d52d411d83ad5f1296a14
MS Seo <msseo@rsupport.com>
2022-06-03 10:12:27 +0900

    [Server] Add resolution! 1920x1080

--- FishServer/src/script/startCamera.sh
+++ FishServer/src/script/startCamera.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-libcamera-vid -n -t 0 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8888/fishtank}' :demux=h264
+libcamera-vid --width 1920 --height 1080 -n -t 0 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8888/fishtank}' :demux=h264
