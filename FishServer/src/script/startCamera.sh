10e11da3886aa01df4ff194dc9b3bc0d07371780
MS Seo <msseo@rsupport.com>
2022-06-03 16:29:56 +0900

    [Server] Camera ffmpeg

--- FishServer/src/script/startCamera.sh
+++ FishServer/src/script/startCamera.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-libcamera-vid --width 1920 --height 1080 -n -t 0 --inline -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8888/fishtank}' :demux=h264
+~/rtsp/rtsp-simple-server
+
+wait 2
+
+libcamera-vid --width 1920 --height 1080 -n -t 0 --inline -o - | ffmpeg -re -i pipe:0 -c:v copy -f rtsp -muxdelay 0.1 rtsp://localhost:8888/fishtank
+
