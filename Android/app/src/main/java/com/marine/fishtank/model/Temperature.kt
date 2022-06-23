e794ab8250645f3af245eb1bda4cce385ffb1a09
MS Seo <msseo@rsupport.com>
2022-06-23 17:22:00 +0900

    [ALL] Temperature.time DataType 변경.

--- Android/app/src/main/java/com/marine/fishtank/model/Temperature.kt
+++ Android/app/src/main/java/com/marine/fishtank/model/Temperature.kt
@@ -2,10 +2,11 @@ package com.marine.fishtank.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.*
 
 @Parcelize
 data class Temperature (
-    val id: Int = 0,
-    val temperature: Float,
-    val time: Long
+    var id: Int = 0,
+    var temperature: Float,
+    var time: Date = Date()
 ): Parcelable
