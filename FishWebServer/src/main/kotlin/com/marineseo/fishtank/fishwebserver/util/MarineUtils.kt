2c36871b544e1ec38eb1877c4e04d3d1c6661c2f
MS Seo <msseo>
2022-06-23 22:59:05 +0900

    [WEB] logback 구현 1차

--- FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/util/MarineUtils.kt
+++ FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/util/MarineUtils.kt
@@ -1,5 +1,8 @@
 package com.marineseo.fishtank.fishwebserver.util
 
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
 object MarineUtils {
@@ -13,5 +16,21 @@ object MarineUtils {
         }
 
         return buffer.toString()
+    }
+}
+
+fun String.runCommand(): String? {
+    try {
+        val parts = this.split("\\s".toRegex())
+        val proc = ProcessBuilder(*parts.toTypedArray())
+            .redirectOutput(ProcessBuilder.Redirect.PIPE)
+            .redirectError(ProcessBuilder.Redirect.PIPE)
+            .start()
+
+        proc.waitFor(60, TimeUnit.MINUTES)
+        return proc.inputStream.bufferedReader().readText()
+    } catch(e: IOException) {
+        e.printStackTrace()
+        return null
     }
 }
