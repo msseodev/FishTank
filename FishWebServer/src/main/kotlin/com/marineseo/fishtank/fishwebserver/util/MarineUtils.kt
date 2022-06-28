add2486f4a85887e8f8bb6d1949f6330804099bf
MS Seo <msseo@rsupport.com>
2022-06-28 16:27:20 +0900

    [Web] Update

--- FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/util/MarineUtils.kt
+++ FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/util/MarineUtils.kt
@@ -5,16 +5,22 @@ import java.io.IOException
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
-object MarineUtils {
-    fun makeRandomString(len: Int): String {
-        val random = Random(System.currentTimeMillis())
-        val buffer = StringBuffer()
+fun makeRandomString(len: Int): String {
+    val random = Random(System.currentTimeMillis())
+    val buffer = StringBuffer()
 
-        repeat(len) {
-            val char = random.nextInt('A'.toInt(), 'z'.toInt()).toChar()
-            buffer.append(char)
-        }
-
-        return buffer.toString()
+    repeat(len) {
+        val char = random.nextInt('A'.toInt(), 'z'.toInt()).toChar()
+        buffer.append(char)
     }
+
+    return buffer.toString()
+}
+
+fun String.runCommand(): String  {
+    val cmds = this.split(" ").toTypedArray()
+    println("cmds=${cmds.joinToString { "[$it]" }}")
+
+    val process = Runtime.getRuntime().exec(cmds)
+    return process.inputStream.bufferedReader().readText()
 }
