ae939b88befd96d36cf331fa801ba3cc8770af82
MS Seo <msseo@rsupport.com>
2022-06-29 15:51:06 +0900

    [Server] TaskService 통합.

--- FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/service/TaskService.kt
+++ FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/service/TaskService.kt
@@ -4,7 +4,13 @@ import com.marineseo.fishtank.fishwebserver.mapper.DatabaseMapper
 import com.marineseo.fishtank.fishwebserver.model.Task
 import com.marineseo.fishtank.fishwebserver.model.Temperature
 import com.marineseo.fishtank.fishwebserver.util.TimeUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationListener
+import org.springframework.context.event.*
 import org.springframework.stereotype.Service
 import java.sql.Date
 import java.sql.Timestamp
@@ -15,13 +21,77 @@ private const val WATER_OUT_IN_MINUTE = 578 // ml
 private const val WATER_REPLACE_RATIO_MIN = 0.5
 
 private const val TAG = "TaskService"
+private const val TASK_INTERVAL = 1000L * 3
 
 @Service
 class TaskService(
+    private val arduinoService: ArduinoService,
     private val mapper: DatabaseMapper
-) {
+) : ApplicationListener<ApplicationContextEvent> {
     private val logger = LoggerFactory.getLogger(this.javaClass)
-    
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private var run = false
+
+    override fun onApplicationEvent(event: ApplicationContextEvent) {
+        logger.warn("onApplicationEvent - $event")
+
+        when (event) {
+            is ContextStartedEvent -> {
+                start()
+            }
+            is ContextRefreshedEvent -> {
+                // Refresh
+                stop()
+                start()
+            }
+            is ContextClosedEvent, is ContextStoppedEvent -> {
+                stop()
+            }
+        }
+    }
+
+    private fun start() {
+        run = true
+        scope.launch {
+            while (run) {
+                fetchTask()?.let { task ->
+                    logger.info("Executing $task")
+
+                    when (task.type) {
+                        Task.TYPE_REPLACE_WATER -> {
+
+                        }
+                        Task.TYPE_VALVE_IN_WATER -> {
+                            arduinoService.enableInWaterValve(
+                                open = task.data == Task.DATA_OPEN
+                            )
+                        }
+                        Task.TYPE_VALVE_OUT_WATER -> {
+                            arduinoService.enableOutWaterValve(
+                                open = task.data == Task.DATA_OPEN
+                            )
+                        }
+                        Task.TYPE_LIGHT -> {
+                            // TODO
+                        }
+                        Task.TYPE_PURIFIER -> {
+                            // TODO
+                        }
+                    }
+
+                    task.state = Task.STATE_FINISH
+                    mapper.updateTask(task)
+                }
+
+                delay(TASK_INTERVAL)
+            }
+        }
+    }
+
+    private fun stop() {
+        run = false
+    }
+
     fun createReplaceWaterTask(ratio: Float) {
         if (ratio < 0 || ratio > WATER_REPLACE_RATIO_MIN) {
             // Wrong param.
@@ -29,7 +99,7 @@ class TaskService(
             return
         }
 
-        if(recentReplaceWaterTaskExist()) {
+        if (recentReplaceWaterTaskExist()) {
             logger.error("Too frequent replacement is not allowed.")
             return
         }
@@ -42,10 +112,12 @@ class TaskService(
 
         // Create tasks.
         // For tracing(and logging), we put TYPE_REPLACE_WATER task.
-        mapper.insertTask(Task(
-            type = Task.TYPE_REPLACE_WATER,
-            state = Task.STATE_STANDBY
-        ))
+        mapper.insertTask(
+            Task(
+                type = Task.TYPE_REPLACE_WATER,
+                state = Task.STATE_STANDBY
+            )
+        )
 
         mapper.insertTask(
             Task(
@@ -85,15 +157,15 @@ class TaskService(
 
     fun recentReplaceWaterTaskExist(): Boolean {
         val replaceTask = mapper.getLastReplaceTask()
-        replaceTask?.let {task ->
-            if(task.executeTime < System.currentTimeMillis() + TimeUtils.MILS_HOUR) {
+        replaceTask?.let { task ->
+            if (task.executeTime < System.currentTimeMillis() + TimeUtils.MILS_HOUR) {
                 return true
             }
         }
         return false
     }
 
-    fun fetchTask(): Task? {
+    private fun fetchTask(): Task? {
         return mapper.fetchTask(Timestamp(System.currentTimeMillis()))
     }
 
