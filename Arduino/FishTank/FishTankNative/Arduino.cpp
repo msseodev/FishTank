9a7bc3f10be507a5f3dceb1fe5369eab8addd48a
MS Seo <msseo@rsupport.com>
2022-08-25 17:11:37 +0900

    [GOOD]

---
+++ Arduino/FishTank/FishTankNative/Arduino.cpp
@@ -0,0 +1,41 @@
+//
+// Created by MSSeo on 2022-08-25.
+//
+
+#include "Arduino.h"
+
+void Ser::println() {
+
+}
+
+void Ser::println(const char *) {
+}
+
+void Ser::print() {}
+
+void Ser::print(const char *) {}
+
+void Ser::print(int) {}
+
+void Ser::print(float) {}
+
+void Ser::print(double) {}
+
+void Ser::print(long) {}
+
+void Ser::write(uint8_t[], int) {}
+
+void Ser::flush() {}
+
+void Ser::readBytes(uint8_t[], int) {}
+
+void Ser::begin(int) {}
+
+void Ser::setTimeout(int) {}
+
+int Ser::read() {}
+
+unsigned long millis(){ }
+void pinMode(int, int){ }
+void digitalWrite(int, int){ }
+void analogWrite(int, int){ }
