2aed127d8ee554cbf35aa103af9486178ad13307
MS Seo <msseo@rsupport.com>
2022-08-31 16:44:30 +0900

    [Server] Packet 구현중...

--- FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/model/FishPacket.kt
+++ FishWebServer/src/main/kotlin/com/marineseo/fishtank/fishwebserver/model/FishPacket.kt
@@ -1,5 +1,10 @@
 package com.marineseo.fishtank.fishwebserver.model
 
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import kotlin.experimental.and
+import kotlin.experimental.or
+
 const val OP_GET_TEMPERATURE: Short = 1000
 const val OP_PIN_IO: Short = 1001
 const val OP_READ_DIGIT_PIN: Short = 1002
@@ -11,6 +16,10 @@ const val PACKET_SIZE = 20
 
 private var autoIncId: Int = 0
 
+const val STX: Byte = 0x02
+const val ETX: Byte = 0x03
+const val DLE: Byte = 0x10
+
 data class FishPacket(
     val id: Int = ++autoIncId,
     val clientId: Int = 0,
@@ -18,12 +27,54 @@ data class FishPacket(
     val pin: Short = 0,
     val pinMode: Short = 0,
     val data: Float = 0f,
-    val crc: Byte = 0
+    var crc: Short = 0
 )
 
+fun FishPacket.makeCrc(): Short {
+    return ((id shl 12).toShort() and 0xF000.toShort()) or
+            ((clientId shl 8).toShort() and 0x0F00) or
+            ((pin.toInt() shl 4).toShort() and 0x00F0) or
+            (pinMode and 0x000F)
+}
 
 fun FishPacket.toRawPacket(): ByteArray {
+    crc = makeCrc()
 
+    val buffer = ByteBuffer.allocate(PACKET_SIZE * 2)
+    buffer.order(ByteOrder.LITTLE_ENDIAN)
+
+    write(id, buffer)
+    write(clientId, buffer)
+    write(opCode, buffer)
+    write(pin, buffer)
+    write(pinMode, buffer)
+    write(data, buffer)
+    write(crc, buffer)
+
+    // TODO Array
+    buffer.limit(buffer.position())
+
+    return buffer.array()
+}
+
+fun write(value: Number, buffer: ByteBuffer) {
+    val size = when(value) {
+        is Long -> Long.SIZE_BYTES
+        is Int -> Int.SIZE_BYTES
+        is Short -> Short.SIZE_BYTES
+        is Double -> Double.SIZE_BYTES
+        else -> 0
+    }
+
+    repeat(size) { index ->
+        val number = value.toInt()
+        val b = number.ushr(index * 8).toByte()
+        when(b) {
+            STX, ETX, DLE -> buffer.put(DLE)
+        }
+
+        buffer.put(b)
+    }
 }
 
 
--- FishWebServer/src/test/kotlin/com/marineseo/fishtank/fishwebserver/FishWebServerApplicationTests.kt
+++ FishWebServer/src/test/kotlin/com/marineseo/fishtank/fishwebserver/FishWebServerApplicationTests.kt
@@ -1,16 +1,37 @@
 package com.marineseo.fishtank.fishwebserver
 
+import com.marineseo.fishtank.fishwebserver.model.FishPacket
+import com.marineseo.fishtank.fishwebserver.model.makeCrc
+import com.marineseo.fishtank.fishwebserver.model.toRawPacket
 import com.marineseo.fishtank.fishwebserver.util.runCommand
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+
 class FishWebServerApplicationTests {
+    private val packet = FishPacket(
+        clientId = 1,
+        id = 2,
+        pin = 10,
+        pinMode = 1
+    )
 
     @Test
     fun generalTest() {
         val routed = "route print".runCommand()
         println(routed)
+    }
+
+    @Test
+    fun crcTest() {
+        println("CRC=${packet.makeCrc()}")
+    }
+
+    @Test
+    fun serializeTest() {
+        val buffer = packet.toRawPacket()
+        println("buffer=$buffer")
+
+
     }
 
 }
