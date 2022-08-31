c549cc27b83f80587c54485fee0bc0f527fd7fe7
MS Seo <msseo@rsupport.com>
2022-08-31 16:08:34 +0900

    [Arduino-Native] FishPacket 분리.

--- Arduino/FishTank/FishTankNative/FishTank.cpp
+++ Arduino/FishTank/FishTankNative/FishTank.cpp
@@ -1,5 +1,6 @@
 #include "DallasTemperature.h"
 #include "Arduino.h"
+#include "FishPacket.h"
 
 
 #define READ_TIMEOUT 5000
@@ -33,148 +34,9 @@ unsigned long prevMils = 0;
 
 int pinState[PIN_LENGTH];
 
-#define STX 0x02
-#define ETX 0x03
-#define DLE 0x10
-
 Ser Serial;
 Ser Serial1;
 
-/**
- * STX - DATA - ETX - CRC
- */
-class FishPacket {
-public:
-    const unsigned char stx = STX;
-
-    // Start Data
-    long id;
-    long clientId;
-    int opCode;
-    int pin;
-    int pinMode;
-    float data;
-    // End Data
-
-    const unsigned char etx = ETX;
-    unsigned int crc;
-
-    /**
-     * Write value to target in little endian order.
-     * Assume value is variable.
-     *
-     * @param sizeOfValue
-     * @param value value to write to target.
-     * @param target target address in buffer.
-     * @return count of byte written in this function.
-     */
-    int write(const unsigned char* value, int sizeOfValue, unsigned char* target) {
-        int idx = 0;
-
-        for(int i=0; i<sizeOfValue; i++) {
-            // Meta data should be escaped.
-            switch (value[i]) {
-                case STX:
-                case ETX:
-                case DLE:
-                    // Add DLE for escape.
-                    target[idx++] = DLE;
-                    break;
-            }
-
-            target[idx++] = value[i];
-        }
-
-        return idx;
-    }
-
-    /**
-     * Serialize this packet to byte array.
-     *
-     * @param bff buffer to serialize.
-     * @return byte size of serialized packet.
-     */
-    int serializePacket(unsigned char bff[]) {
-        // Little-endian
-        int index = 0;
-        bff[index++] = STX;
-
-        index += write((unsigned char *)&id, 4, bff+index);
-        index += write((unsigned char *)&clientId, 4, bff+index);
-        index += write((unsigned char *)&opCode, 2, bff+index);
-        index += write((unsigned char *)&pin, 2, bff+index);
-        index += write((unsigned char *)&pinMode, 2, bff+index);
-        index += write((unsigned char *)&data, 4, bff+index);
-
-        bff[index++] = ETX;
-        bff[index++] = makeCrc();
-
-        return index;
-    }
-
-    /**
-     *
-     * @param value
-     * @param sizeOfValue
-     * @param buffer
-     * @return byte size of read from buffer.
-     */
-    int read(unsigned char* value, int sizeOfValue, const unsigned char* buffer) {
-        int idx = 0;
-        int vIdx = 0;
-
-        while(true) {
-            unsigned char b = buffer[idx++];
-            if(b == DLE) {
-                continue;
-            }
-
-            value[vIdx++] = b;
-            if(vIdx >= sizeOfValue) break;
-        }
-        return idx;
-    }
-
-    int deSerializePacket(unsigned char bff[]) {
-        // Little-endian
-        int index = 0;
-        if(bff[index++] != STX) return -1;
-
-        index += read((unsigned char *)&id, 4, bff+index);
-        index += read((unsigned char *)&clientId, 4, bff+index);
-        index += read((unsigned char *)&opCode, 2, bff+index);
-        index += read((unsigned char *)&pin, 2, bff+index);
-        index += read((unsigned char *)&pinMode, 2, bff+index);
-        index += read((unsigned char *)&data, 4, bff+index);
-
-        if(bff[index++] != ETX) return -1;
-        crc = bff[index++];
-
-        return index;
-    }
-
-    void clear() {
-        id = 0;
-        clientId = 0;
-        opCode = 0;
-        pin = 0;
-        pinMode = 0;
-        data = 0;
-        crc = 0;
-    }
-
-    bool validateCrc() {
-        return makeCrc() == crc;
-    }
-
-    unsigned int makeCrc() {
-        int localCrc = (id << 12) & 0xF000;
-        localCrc |= (clientId << 8) & 0x0F00;
-        localCrc |= (pin << 4) & 0x00F0;
-        localCrc |= pinMode & 0x000F;
-        return localCrc;
-    }
-};
 
 void clearPacket(FishPacket &packet) {
     packet.id = 0;
--- Arduino/FishTank/FishTankNative/main.cpp
+++ Arduino/FishTank/FishTankNative/main.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-
+#include "FishPacket.h"
 
 using namespace std;
 
-unsigned int makeCrc(long id, long clientId, int pin, int pinMode) {
-    int crc = (id << 12) & 0xF000;
-    crc |= (clientId << 8) & 0x0F00;
-    crc |= (pin << 4) & 0x00F0;
-    crc |= pinMode & 0x000F;
-    return crc;
-}
-
 int main() {
-    int crc = makeCrc(1,5,2,1);
+    auto* fishPacket = new FishPacket();
+    fishPacket->clientId = 1;
+    fishPacket->id = 2;
+    fishPacket->pin = 10;
+    fishPacket->pinMode = 1;
+
+    unsigned int crc = fishPacket->makeCrc();
 
     cout << "CRC=" << crc;
 
