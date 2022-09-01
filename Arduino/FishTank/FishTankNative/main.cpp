841ece29d3c386af32d3d5e7cc847523c0195d30
MS Seo <msseo@rsupport.com>
2022-09-01 14:21:22 +0900

    [Arduino-Native] Packet 구현.

--- Arduino/FishTank/FishTankNative/main.cpp
+++ Arduino/FishTank/FishTankNative/main.cpp
@@ -3,16 +3,37 @@
 
 using namespace std;
 
+void printPacket(FishPacket* packet) {
+    cout << "id=" << packet->id << " clientId=" << packet->clientId <<
+        " opCode=" << packet->opCode <<
+         " pin=" << packet->pin << " pinMode=" << packet->pinMode <<
+         " data=" << packet->data << " crc=" << packet->crc <<
+         " valid=" << packet->validateCrc() <<
+         endl;
+}
+
 int main() {
-    auto* fishPacket = new FishPacket();
-    fishPacket->clientId = 1;
-    fishPacket->id = 2;
-    fishPacket->pin = 10;
-    fishPacket->pinMode = 1;
+    for(int i=0; i<2000; i++) {
+        auto *fishPacket = new FishPacket();
+        fishPacket->clientId = i;
+        fishPacket->id = i+100;
+        fishPacket->pin = i+299;
+        fishPacket->pinMode = i;
+        fishPacket->opCode = i;
+        fishPacket->data = i * 0.75;
 
-    unsigned int crc = fishPacket->makeCrc();
+        unsigned int crc = fishPacket->makeCrc();
+        cout << "CRC=" << crc << endl;
 
-    cout << "CRC=" << crc;
+        unsigned char buffer[40];
+        int packetSize = fishPacket->serializePacket(buffer);
+        cout << "PacketSize=" << packetSize << endl;
+        printPacket(fishPacket);
+
+        auto *aPacket = new FishPacket();
+        aPacket->deSerializePacket(buffer);
+        printPacket(aPacket);
+    }
 
     return 0;
 }
