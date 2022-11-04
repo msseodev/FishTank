8307033878d14826160553c1ed6b08d1fe75a303
MS Seo <msseo@rsupport.com>
2022-11-04 17:22:17 +0900

    Tab pager -> Bottom navigation.

--- Android/MPChartLib/src/main/java/com/github/mikephil/charting/charts/Chart.java
+++ Android/MPChartLib/src/main/java/com/github/mikephil/charting/charts/Chart.java
@@ -21,6 +21,7 @@ import androidx.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
