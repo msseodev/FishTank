ea8514344ab12c31ae41985e945d70b85dd3c6fd
MS Seo <msseo@rsupport.com>
2023-03-08 11:08:45 +0900

    [Server] JPA camelcase Naming Strategy

---
+++ FishWebServer/web/src/main/kotlin/com/marineseo/fishtank/jpa/CamelCaseNamingStrategy.kt
@@ -0,0 +1,35 @@
+package com.marineseo.fishtank.jpa
+
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy
+import org.hibernate.boot.model.naming.Identifier
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment
+
+class CamelCaseNamingStrategy: CamelCaseToUnderscoresNamingStrategy() {
+    override fun toPhysicalTableName(name: Identifier, jdbcEnvironment: JdbcEnvironment): Identifier {
+        return Identifier(name.text.toCamelCase(), name.isQuoted)
+    }
+
+    override fun toPhysicalColumnName(name: Identifier, jdbcEnvironment: JdbcEnvironment): Identifier {
+        return Identifier(name.text.toCamelCase(), name.isQuoted)
+    }
+
+
+}
+
+fun String.toCamelCase(): String {
+    val result = StringBuilder()
+    var nextIsUpper = false
+    for (ch in this) {
+        if (ch == '_') {
+            nextIsUpper = true
+        } else {
+            if (nextIsUpper) {
+                result.append(ch.uppercaseChar())
+                nextIsUpper = false
+            } else {
+                result.append(ch.lowercaseChar())
+            }
+        }
+    }
+    return result.toString()
+}
