561386d17b72d402c6ed22431860d83aabf101e5
MS Seo <msseo@rsupport.com>
2023-03-17 15:06:07 +0900

    [Android] Change log.

--- Android/app/src/main/java/com/marine/fishtank/logger/PlainLogger.kt
+++ Android/app/src/main/java/com/marine/fishtank/logger/PlainLogger.kt
@@ -5,8 +5,13 @@ import com.orhanobut.logger.FormatStrategy
 
 class PlainLogger: FormatStrategy {
     override fun log(priority: Int, tag: String?, message: String) {
-        val className = Exception().stackTrace[1].className.substringAfterLast(".")
-        val threadName = Thread.currentThread().name
+        val curThread = Thread.currentThread()
+
+        val className = curThread.stackTrace.find {
+            !it.className.endsWith("com.marine.fishtank.logger.PlainLogger") &&
+            it.className.contains("com.marine.fishtank")
+        }?.className?.substringAfterLast(".")
+        val threadName = curThread.name
 
         Log.println(priority, className, "t=$threadName msg=$message")
     }
